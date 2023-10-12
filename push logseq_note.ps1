8c8f9a816b4e648d923e9258216a705405126c9f
hxse <narutohy@163.com>
2023-10-12 16:37:35 +0800

    auto update

--- push logseq_note.ps1
+++ push logseq_note.ps1
@@ -1,11 +1,20 @@
 
 git remote set-url origin https://hxse:%note_github_token%@github.com/hxse/logseq_note.git
-git pull --no-rebase
+
+$output = git pull --no-rebase
+if($output.Contains("Already up to date.")){
+    echo "---- no need pull"
+}
+else{
+    echo "---- yes need pull"
+}
+    
 git add .
 git diff-index --quiet HEAD --
-
 if ($?) {
-    echo no different
+	echo "---- no need push"
 } else {
-    echo check different
+	echo "---- yes need push"
+    git commit -m "auto update"
+    git push
 }
