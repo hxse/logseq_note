9105c18fae818b61e2f8de2c79c58d29bfef8f03
hxse <narutohy@163.com>
2023-10-12 18:45:09 +0800

    auto update

--- sync logseq_note.ps1
+++ sync logseq_note.ps1
@@ -3,41 +3,53 @@ param (
     [bool] $auto = $false,
     [int] $sleep = 10
 )
-echo $dir
-echo $auto
-echo $sleep
+
 cd $dir
 
 $token=$env:note_github_token
 git remote set-url origin https://hxse:$token@github.com/hxse/logseq_note.git
 
-$output = git pull --no-rebase
-echo "---- check remote"
+function Start-GitAutoCommitAndPush {
+	$output = git pull --no-rebase
+	echo "---- check remote"
 
-git add .
-git diff-index --quiet HEAD --
-if ($?) {
-    if($output.Contains("Already up to date.")){
-        echo "---- nothing"
-	}
-	else{
-		echo "---- pull"
-	}
-} else {
-    
-    if($output.Contains("Already up to date.")){
-        echo "---- push"
-        git commit -m "auto update"
-		git push
-	}
-	else{
-		echo "---- merge"
-		echo $output
-		git commit -m "auto update"
+	git add .
+	git diff-index --quiet HEAD --
+	if ($?) {
+		if($output.Contains("Already up to date.")){
+			echo "---- nothing"
+		}
+		else{
+			echo "---- pull"
+		}
+	} else {
+		
+		if($output.Contains("Already up to date.")){
+			echo "---- push"
+			git commit -m "auto update"
+			git push
+		}
+		else{
+			echo "---- merge"
+			echo $output
+			git commit -m "auto update"
 
-		git pull --no-rebase
-		git add .
-		git commit -m "auto merge"
-		git push
+			git pull --no-rebase
+			git add .
+			git commit -m "auto merge"
+			git push
+		}
 	}
+}
+
+if ($auto) {
+    while ($true) {
+        Get-Date
+        Start-GitAutoCommitAndPush
+        Start-Sleep -Seconds $sleep
+    }
+}
+else {
+    Get-Date
+    Start-GitAutoCommitAndPush
 }
