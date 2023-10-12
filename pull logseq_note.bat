0ed37a5760f6cc7bd0ed113485d1f966639ce5c9
hxse <narutohy@163.com>
2023-10-12 13:34:06 +0800

    auto update

---
+++ pull logseq_note.bat
@@ -0,0 +1,3 @@
+git remote set-url origin https://hxse:%note_github_token%@github.com/hxse/logseq_note.git
+git pull
+pause
---
+++ push logseq_note.bat
@@ -0,0 +1,26 @@
+chcp 65001
+cd %~dp0
+@echo off
+:loop
+
+git add .
+git diff-index --quiet HEAD --
+
+if %errorlevel% equ 0 (
+    echo no different
+) else (
+    echo check different
+    git commit -m "auto update"
+    git remote set-url origin https://hxse:%note_github_token%@github.com/hxse/logseq_note.git
+    git push
+)
+
+
+set /a s=30
+echo:
+echo:
+echo wait %s% second
+echo:
+echo:
+timeout /t %s% >nul
+goto loop
