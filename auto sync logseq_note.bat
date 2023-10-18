84e872d99138e497e4af86a455e232dd316e8e07
hxse <narutohy@163.com>
2023-10-18 15:55:00 +0800

    auto update

--- auto sync logseq_note.bat
+++ auto sync logseq_note.bat
@@ -1,3 +1,8 @@
 cd %~dp0
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './sync logseq_note.ps1' -d '%~dp0' -a 1 -s 15"
+if "%~1" == "" (
+    SET /A sleep = 20
+) else (
+    SET /A sleep = %1
+)
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './sync logseq_note.ps1' -d '%~dp0' -a 1 -s %sleep%"
 pause
